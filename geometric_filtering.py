bd6b8b09ca195587133b151b24a57bac41798581
andrearigo-dev <andre.rg96@gmail.com>
2021-08-24 16:55:11 +0200

    Added arms and legs

--- geometric_filtering.py
+++ geometric_filtering.py
@@ -31,25 +31,61 @@ def is_in_truncated_cylinder(point, x1, x2, r):
 
 
 def filter(pcd, skels, edges):
-    # TODO
-    # for each point in the pointcloud, check if it
-    # belongs to the truncated cylinder defined between
-    # the two points of each each edge
-    
-    # lElbow, lWrist
-    edge = [4,5]
 
-    x1 = skels[0][edge[0]]
-    x2 = skels[0][edge[1]]
+    # 0: Neck
+    # 1: Nose
+    # 2: BodyCenter (center of hips)
+    # 3: lShoulder
+    # 4: lElbow
+    # 5: lWrist,
+    # 6: lHip
+    # 7: lKnee
+    # 8: lAnkle
+    # 9: rShoulder
+    # 10: rElbow
+    # 11: rWrist
+    # 12: rHip
+    # 13: rKnee
+    # 14: rAnkle
+    # 15: lEye
+    # 16: lEar
+    # 17: rEye
+    # 18: rEar
 
-    n = x1-x2
-
-    r = 5
-
+    skel = skels[0]
     indexes = []
 
+
     for i, point in enumerate(pcd.points):
-        if is_in_truncated_cylinder(point, x1, x2, r):
+        if (
+            # ARMS
+
+            # lShoulder, lElbow: 3,4
+            is_in_truncated_cylinder(point, skel[3], skel[4], 10)
+
+            # lElbow, lWrist: 4,5
+            or is_in_truncated_cylinder(point, skel[4], skel[5], 5)
+
+            # rShoulder, rElbow: 9,10
+            or is_in_truncated_cylinder(point, skel[9], skel[10], 10)
+
+            # rElbow, rWrist: 10,11
+            or is_in_truncated_cylinder(point, skel[10], skel[11], 5)
+
+            # LEGS
+
+            # lHip, lKnee: 6,7
+            or is_in_truncated_cylinder(point, skel[6], skel[7], 10)
+
+            # lKnee, lAnkle: 7,8
+            or is_in_truncated_cylinder(point, skel[7], skel[8], 10)
+
+            # rHip, rKnee: 12,13
+            or is_in_truncated_cylinder(point, skel[12], skel[13], 10)
+
+            # rKnee, kAnkle: 13,14
+            or is_in_truncated_cylinder(point, skel[13], skel[14], 10)
+        ):
             indexes.append(i)
 
     return pcd.select_by_index(indexes)
