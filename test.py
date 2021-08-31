84d5be2caef52943e6ffd11fd705ab8333b2bb44
andrearigo-dev <andre.rg96@gmail.com>
2021-08-31 16:30:01 +0200

    More tests

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 import open3d as o3d
+import numpy as np
 
-cyl = o3d.geometry.create_mesh_cylinder(radius=1.0, height=3.0)
-# cyl_scaled = cyl
-# cyl_scaled.scale(2)
 
-# o3d.visualization.draw_geometries([cyl])
+box = o3d.geometry.TriangleMesh.create_box(width=1.0, height=2.0, depth=4.0)
+box2 = o3d.geometry.TriangleMesh.create_box(width=1.0, height=2.0, depth=4.0)
+
+R = o3d.geometry.get_rotation_matrix_from_axis_angle(rotation=np.array([0.3, 0.3, 0.3]))
+
+box.rotate(R)
+box2.rotate(R)
+
+box2.vertices = o3d.utility.Vector3dVector(np.asarray(box2.vertices) * np.array([1., 2., 4.]))
+box2.translate(translation=np.array([3,3,3]))
+
+o3d.visualization.draw_geometries([box, box2])
