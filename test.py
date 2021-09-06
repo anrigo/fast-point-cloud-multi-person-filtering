1f48c7b103cf4a59dd48d4fa3bf8a237e53113d9
andrearigo-dev <andre.rg96@gmail.com>
2021-09-06 14:21:47 +0200

    Test bounding box

--- test.py
+++ test.py
@@ -6,19 +6,51 @@ def aaa(pt):
     print("({},{},{})".format(pt[0], pt[1], pt[2]))
 
 
-A = np.array([2,2,0])
-l = np.linalg.norm(A)
+# A = np.array([2,2,0])
+# l = np.linalg.norm(A)
 
 
-# theta_x = np.pi/4
-theta_x = np.arccos(A[0]/l)
-theta_y = np.arccos(A[1]/l)
-theta_z = np.arccos(A[2]/l)
-print(theta_x)
+# # theta_x = np.pi/4
+# theta_x = np.arccos(A[0]/l)
+# theta_y = np.arccos(A[1]/l)
+# theta_z = np.arccos(A[2]/l)
+# print(theta_x)
 
-R = o3d.geometry.get_rotation_matrix_from_axis_angle([0,0,-theta_x])
-print(R)
-# R_inv = np.linalg.inv(R)
+# R = o3d.geometry.get_rotation_matrix_from_axis_angle([0,0,-theta_x])
+# print(R)
+# # R_inv = np.linalg.inv(R)
 
-V = np.dot(R, np.array([2,2,0]))
-aaa(V)
+# V = np.dot(R, np.array([2,2,0]))
+# aaa(V)
+
+A = np.array([1,1,1])
+B = np.array([3,3,3])
+
+
+v = B-A
+l = np.linalg.norm(v)
+
+rot = np.arccos(v/l)
+print(rot)
+
+R = o3d.geometry.get_rotation_matrix_from_axis_angle([0.000001,0.000001,0.000001])
+# R = o3d.geometry.get_rotation_matrix_from_axis_angle(rot)
+
+box = o3d.geometry.OrientedBoundingBox(v, R, [1,1,1])
+
+
+v3d = o3d.utility.Vector3dVector([A,B])
+pcd = o3d.geometry.PointCloud(v3d)
+mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.6, origin=[0,0,0])
+mesh_frame2 = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.3, origin=v)
+mesh_frame2.rotate(R)
+box_vis = o3d.geometry.PointCloud(box.get_box_points())
+
+line_set = o3d.geometry.LineSet.create_from_oriented_bounding_box(box)
+
+lines = [[0,1]]
+line_set2 = o3d.geometry.LineSet()
+line_set2.points = o3d.utility.Vector3dVector([A,B])
+line_set2.lines = o3d.utility.Vector2iVector(lines)
+
+o3d.visualization.draw_geometries([pcd, mesh_frame, box_vis, line_set, mesh_frame2, line_set2])
