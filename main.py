e857e950283e13a1b2997fab4dbe65ba09c1859f
andrearigo-dev <andre.rg96@gmail.com>
2021-10-13 11:54:29 +0200

    Added new multi-person point clouds

--- main.py
+++ main.py
@@ -67,9 +67,9 @@ def load_ptcloud(path, draw=False):
 
 
 if __name__ == "__main__":
-    pcd = load_ptcloud("kinoptic_ptclouds/171204_pose1/ptcloud_hd00000135.ply")
+    pcd = load_ptcloud("kinoptic_ptclouds/170407_haggling_a1/ptcloud_hd00001651.ply", draw=True)
 
-    skels = load_skeleton_points_as_nparray('171204_pose1', 135)
+    skels = load_skeleton_points_as_nparray('170407_haggling_a1', 1651)
 
     # head = np.array([[-126.85966667, -163.11133333, -11.09929333]])
 
